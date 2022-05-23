0790086efd112ce9e580f1d3aed4b431773f2b13
luisnrq <luisenrique.frias@tajamar365.com>
2022-05-23 02:42:07 +0200

    commit

---
+++ Pagina.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MadTechAWS
+{
+    [Table("PAGINAS")]
+    public class Pagina
+    {
+        [Column("ID_PAGINA")]
+        public int IdPagina { get; set; }
+
+        [Column("TITULO ")]
+        public string Titulo { get; set; }
+
+        [Column("CONTENIDO")]
+        public string Contenido { get; set; }
+    }
+}
